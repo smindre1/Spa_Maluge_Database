143cba694ef1ca22508a376cdc2e639aefbed116
Shane Mindreau <smindre1@binghamton.edu>
2024-05-01 23:20:33 -0400

    I cleaned up the server.js by deleting out the commented out code.

--- server.js
+++ server.js
@@ -11,15 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-// if we're in production, serve client/dist as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/dist')));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-//   });
-// }
-
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
