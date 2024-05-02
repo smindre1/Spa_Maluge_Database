55174715f340306a6e3fe5f0aed658bed4379300
Shane Mindreau <smindre1@binghamton.edu>
2024-05-02 01:05:57 -0400

    I have updated the server.js file to use cors middleware which should allow me to control which sites can make requests to this api through an env variable called 'ORIGIN'.

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ require('dotenv').config();
 const db = require('./config/connection');
 const routes = require('./routes');
 const { authMiddleware } = require("./utils/auth");
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Use the cors middleware and configure it to allow requests from specific origins
+app.use(cors({ 
+  origin: process.env.ORIGIN,
+}));
+
 app.use(routes);
 
 db.once('open', () => {
